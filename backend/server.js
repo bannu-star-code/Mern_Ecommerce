8666068eb2599b4b159b73421229e54333d093d0
bannu-star-code <nitinchiliveri007@gmail.com>
2026-05-28 21:47:20 +0530

    Added Comments

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Added in Git
 // CORS Configuration
 app.use(cors({
     origin: process.env.CLIENT_URL || "http://localhost:5173",
